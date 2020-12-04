7946ae2a709484dee739807e8a5bbbb4d2b92689
Roman Batin <roman.batin@qoollo.com>
2020-12-04 15:30:26 +0300

    some additions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   tileUrl: 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
   anotherTileUrl: 'http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',
-  reverseGeocodingUrl: 'https://nominatim.openstreetmap.org/reverse'
+  reverseGeocodingUrl: 'https://nominatim.openstreetmap.org/reverse',
+  mapCenter: [63.9373, 92.6550]
 };
 
